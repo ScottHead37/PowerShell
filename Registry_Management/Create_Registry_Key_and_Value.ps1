ca83dfd4a1b9c4e2e9ea837f4dc430c2b12c330f
Scott Head <ScottHead37@gmail.com>
2023-05-25 17:17:56 -0700

    omg

--- Registry_Management/Create_Registry_Key_and_Value.ps1
+++ Registry_Management/Create_Registry_Key_and_Value.ps1
@@ -3,15 +3,28 @@
     Script to create new registry key.
 
     .DESCRIPTION    
-    This checks for key path and then creates the key path if needed.
-    This is used to create registry value on multiple remote systems.
+    This used to check for key path and then create the key path.
+    invoke-command statment to create registry values on multiple remote systems.
 
     .EXAMPLE
     Fill in the values as shown below in the script to customize your registry key.   
-    KeyPath1='HKLM:\HARDWARE\DESCRIPTION\System'  
-    MyNewKeyName='MyNewKeyName'  
-    MyNewKeytysValue='245'  
-    MyKeyDataType='DWORD' 
+    $KeyPath1="HKLM:\HARDWARE\DESCRIPTION\System"   
+    $MyNewKeyName="MyNewKeyName"   
+    $MyNewKeytysValue="245"   
+    $MyKeyDataType="DWORD"
+
+    .INPUTS
+    Get data from source file with sysytems separated by carriage returns 
+    Get-Content C:\temp\Comp.txt
+
+    .OUTPUTS
+    Logging files
+    #Export WinRM Failures to txt
+    $WinRMErrorVar | Out-File C:\temp\RegWinRMFailures.txt 
+    #Export Ping Failures to txt 
+    $Array_PingFailed | Out-File C:\temp\RegPingFailed.txt 
+    C:\Temp\RegSumOfSystemsNotProcessed.txt
+    C:\temp\RegFinalScriptErrors.txt
 
     .NOTES
     !!!! You must use the prefix abbreviations as show below: !!!!
@@ -21,6 +34,7 @@
     HKEY_CURRENT_CONFIG (HKCC)
     HKEY_CLASSES_ROOT (HKCR)
     HKEY_USERS (HKU)
+    HKEY_CURRENT_USER (HKCU)
     ! You must declare a valid data value type !
     BINARY
     DWORD
@@ -32,18 +46,42 @@
     .LINK
     Online Website: https://www.scriptsbyscott.com
 #> 
+#Get Computers from File
+$ArrayofComputers = Get-Content C:\temp\Comp.txt
 
-#Object that is passed to remote systems
+#Summary of Ping Failures
+$Array_PingFailed = @()
+#Summary of Systems Pass Ping and WinRM
+$Array_Passed = @()
+#Summary of errors when tryin to connect to online system via WinRM
+$WinRMErrorVar = @() 
+
+#Connection Testing
+Foreach ($Computer in $ArrayofComputers) {
+    $Checker = Test-Connection $Computer.Trim() -Count 1 -Quiet    
+    If ($Checker) { $Array_Passed += Invoke-Command -ErrorVariable MyErr -ErrorAction SilentlyContinue -ComputerName $Computer.Trim() -ScriptBlock { $Env:COMPUTERNAME } }Else { $Array_PingFailed += $Computer.trim() }
+    $WinRMErrorVar += $MyErr 
+}
+
+#Export WinRM Failures to txt
+$WinRMErrorVar | Out-File C:\temp\RegWinRMFailures.txt 
+#Export Ping Failures to txt 
+$Array_PingFailed | Out-File C:\temp\RegPingFailed.txt 
+
+#Sum on inaccessible systems are not getting processed 
+Compare-Object -ReferenceObject $Array_Passed -DifferenceObject $ArrayofComputers | Select -ExpandProperty inputobject | Out-File C:\Temp\RegSumOfSystemsNotProcessed.txt
+
+#Object to be passed in array to comptuers
 $MyObj = [PSCustomObject]@{
-    KeyPath1         = 'HKLM:\HARDWARE\DESCRIPTION\System25'  
+    KeyPath1         = 'HKLM:\HARDWARE\DESCRIPTION\SystemSS'  
     MyNewKeyName     = 'MyNewKeyName'  
     MyNewKeytysValue = '245'  
     MyKeyDataType    = 'DWORD' 
 }    
 
-#Script that is excuted on remote systems
+#Script Executed on Remote Machines 
 $MyScript = {
-
+    
     Try { 
         #Makes sure the path is set before creation of reg key 
         if (Test-Path $Args.KeyPath1) { 
@@ -61,7 +99,8 @@ $MyScript = {
     }
 }
 
-#Command that executes the MyScript section and can 
-#easily be modified to run on many systems just change
-#"Client" to a list of computer names stored in a variable.
-Invoke-Command -ComputerName "Client" -ScriptBlock $MyScript -ArgumentList $MyObj
+#Command to Exexute Scriptbock $MyScript 
+Invoke-Command -ErrorVariable MyRegErr -ErrorAction SilentlyContinue -ComputerName $Array_Passed -ScriptBlock $MyScript -ArgumentList $MyObj
+
+#Output Errors to TXT Files 
+if ($MyRegErr -ne $null) { $My2ndErr | Out-File C:\temp\RegFinalScriptErrors.txt }
