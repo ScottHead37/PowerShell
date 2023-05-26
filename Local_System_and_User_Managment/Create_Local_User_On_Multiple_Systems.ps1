4f854b8d6aa0b2e7883caa2c4bba3d40b9668652
Scott Head <ScottHead37@gmail.com>
2023-05-25 17:12:02 -0700

    Updated

--- Local_System_and_User_Managment/Create_Local_User_On_Multiple_Systems.ps1
+++ Local_System_and_User_Managment/Create_Local_User_On_Multiple_Systems.ps1
@@ -1,39 +1,51 @@
-<#
-        .SYNOPSIS
-        Creates local user account on remote systems
+<# 
+    .SYNOPSIS
+    Script to create new registry key.
 
-        .DESCRIPTION
-        Used to create a local user account on remote systems with the same 
-        user name and password. This can be for a auto logon account, backup 
-        local admin account
-        
-        .INPUTS
-        You need to update the script to meet your needs.
-        Input script should be a list of your computers 
-        and should reside in C:\temp on the system you 
-        run the script.
+    .DESCRIPTION    
+    This used to check for key path and then create the key path.
+    invoke-command statment to create registry values on multiple remote systems.
 
-        .OUTPUTS
-        #Export WinRM Failures to txt
-        $WinRMErrorVar | Out-File C:\temp\WinRMFailures.txt 
-        
-        #Export Ping Failures to txt 
-        $Array_PingFailed | Out-File C:\temp\PingFailed.txt 
-        
-        #Sum on inaccessible systems are not getting processed 
-        C:\Temp\SumOfSystemsNotProcessed.txt
+    .EXAMPLE
+    Fill in the values as shown below in the script to customize your registry key.   
+    $KeyPath1="HKLM:\HARDWARE\DESCRIPTION\System"   
+    $MyNewKeyName="MyNewKeyName"   
+    $MyNewKeytysValue="245"   
+    $MyKeyDataType="DWORD"
 
-        #Script Results Output
-        C:\temp\ScriptOutput.csv
+    .INPUTS
+    Get data from source file with sysytems separated by carriage returns 
+    Get-Content C:\temp\Comp.txt
 
-        #script Error Output
-        C:\temp\FinalScriptErrors.txt
+    .OUTPUTS
+    Logging files
+    #Export WinRM Failures to txt
+    $WinRMErrorVar | Out-File C:\temp\RegWinRMFailures.txt 
+    #Export Ping Failures to txt 
+    $Array_PingFailed | Out-File C:\temp\RegPingFailed.txt 
+    C:\Temp\RegSumOfSystemsNotProcessed.txt
+    C:\temp\RegFinalScriptErrors.txt
 
-         .LINK
-        Online Website: https://www.scriptsbyscott.com
+    .NOTES
+    !!!! You must use the prefix abbreviations as show below: !!!!
+    Incorrect Syntax:    HKEY_LOCAL_MACHINE\HARDWARE\DESCRIPTION\System
+    Correct Syntax:      HKLM:\HARDWARE\DESCRIPTION\System  
+    HKEY_LOCAL_MACHINE (HKLM)
+    HKEY_CURRENT_CONFIG (HKCC)
+    HKEY_CLASSES_ROOT (HKCR)
+    HKEY_USERS (HKU)
+    HKEY_CURRENT_USER (HKCU)
+    ! You must declare a valid data value type !
+    BINARY
+    DWORD
+    STRING
+    MULTISTRING
+    EXPANDSTRING
+    QWORD
 
-    #>
-
+    .LINK
+    Online Website: https://www.scriptsbyscott.com
+#> 
 #Get Computers from File
 $ArrayofComputers = Get-Content C:\temp\Comp.txt
 
@@ -52,22 +64,43 @@ Foreach ($Computer in $ArrayofComputers) {
 }
 
 #Export WinRM Failures to txt
-$WinRMErrorVar | Out-File C:\temp\WinRMFailures.txt 
+$WinRMErrorVar | Out-File C:\temp\RegWinRMFailures.txt 
 #Export Ping Failures to txt 
-$Array_PingFailed | Out-File C:\temp\PingFailed.txt 
+$Array_PingFailed | Out-File C:\temp\RegPingFailed.txt 
 
 #Sum on inaccessible systems are not getting processed 
-Compare-Object -ReferenceObject $Array_Passed -DifferenceObject $ArrayofComputers | Select -ExpandProperty inputobject | Out-File C:\Temp\SumOfSystemsNotProcessed.txt
+Compare-Object -ReferenceObject $Array_Passed -DifferenceObject $ArrayofComputers | Select -ExpandProperty inputobject | Out-File C:\Temp\RegSumOfSystemsNotProcessed.txt
 
-#Script Command to Create Account
+#Object to be passed in array to comptuers
+$MyObj = [PSCustomObject]@{
+    KeyPath1         = 'HKLM:\HARDWARE\DESCRIPTION\SystemSS'  
+    MyNewKeyName     = 'MyNewKeyName'  
+    MyNewKeytysValue = '245'  
+    MyKeyDataType    = 'DWORD' 
+}    
+
+#Script Executed on Remote Machines 
 $MyScript = {
-
-    $MyPassword = "P@ssword1" | ConvertTo-SecureString -AsPlainText -Force 
-    New-LocalUser -Name "BackupAdmin1" -Password $MyPassword -FullName "Scott Head" -Description "Backup Admin Account"
+    
+    Try { 
+        #Makes sure the path is set before creation of reg key 
+        if (Test-Path $Args.KeyPath1) { 
+            New-ItemProperty -Path $Args.KeyPath1 -Name $Args.MyNewKeyName -Value $Args.MyNewKeytysValue -PropertyType $Args.MyKeyDataType -Force -ErrorAction Stop | Out-Null 
+            return '$Env:COMPUTERNAME - $KeyPath1 path found and key created' 
+        } 
+        else { 
+            New-Item -Path $Args.KeyPath1 -Force | Out-Null -ErrorAction Continue 
+            New-ItemProperty -Path $Args.KeyPath1 -Name $Args.MyNewKeyName -Value $Args.MyNewKeytysValue -PropertyType $Args.MyKeyDataType -Force -ErrorAction Stop | Out-Null -ErrorAction Continue 
+            return '$Env:COMPUTERNAME - $KeyPath1 path had to be created and key value was set' 
+        } 
+    } 
+    Catch { 
+        return $_.Exception 
+    }
 }
 
-#Command that Creates Account and Export Results 
-Invoke-Command -ErrorVariable My2ndErr -ErrorAction SilentlyContinue -ComputerName $Array_Passed -ScriptBlock $MyScript `
-| Select FullName, Enabled, PasswordChangeableDate, PasswordExpires, UserMayChangePassword, PasswordLastSet, Name, SID | Export-csv C:\temp\ScriptOutput.csv -NoTypeInformation
+#Command to Exexute Scriptbock $MyScript 
+Invoke-Command -ErrorVariable MyRegErr -ErrorAction SilentlyContinue -ComputerName $Array_Passed -ScriptBlock $MyScript -ArgumentList $MyObj
 
-if ($My2ndErr -ne $null) { $My2ndErr | Out-File C:\temp\FinalScriptErrors.txt }
+#Output Errors to TXT Files 
+if ($MyRegErr -ne $null) { $My2ndErr | Out-File C:\temp\RegFinalScriptErrors.txt }
